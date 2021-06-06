6dd16450fd27df06115f43399a54b240c61fd086
anpanman-io <mariomuharjani@gmail.com>
2021-06-06 21:29:09 +0700

    commit1

---
+++ app/src/main/java/com/example/a10118390_baru/Profile.java
@@ -0,0 +1,25 @@
+package com.example.a10118390_baru;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+//06-06-2021 - 10118390 - Mario Gonzaga Muharjani - IF-9
+
+public class Profile extends Fragment {
+    public Profile() {
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_profile, container, false);
+    }
+}
